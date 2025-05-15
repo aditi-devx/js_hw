f24628f4325cc86b8b83e25270ca188f6371616d
aditi <aditisingh100406@gmail.com>
2025-05-16 03:17:47 +0530

    js for,while loop

---
+++ hw.js
@@ -0,0 +1,51 @@
+let favcolor ="Whine red";
+console.log (`my fav color is ${favcolor}`);
+
+
+
+let age="19"
+if(age>=19){ 
+    console.log("can vote");
+}else{
+    console.log("cannot vote");
+}
+
+let voterage= 20;
+function canvote(voterage){  //if age is >18 then print can vote
+    if(voterage=>18){
+        return true 
+    }
+    else{
+        return false
+    }
+}
+
+
+console.log (`my fav color is ${favcolor}`);
+console.log (`my height is ${heightincm}`);
+pizza();
+pastry();
+names("aditi", "shubh");
+
+
+let num="10"
+if(num%2===0){  //even odd numbers
+    console.log("even")
+}
+else{
+    console.log("odd")
+}
+
+let integer = 0
+while (integer < 10) {
+    console.log("the number is" + integer)// using while loop for printing numbers from 1 to 10
+    integer++;
+}
+
+
+
+let cake = 20
+for (let i = 0; i <= cake; i++) {
+    console.log(" i  will give shubh " + i + "cake")
+}
+giti
